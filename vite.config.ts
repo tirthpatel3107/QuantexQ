c7f10c4fe4cec34b5d99b870980f34e70e6ead28
Tirth Dhamsania <tirthpatel3107@gmail.com>
2026-02-10 19:20:47 +0530

    Add history api for the fallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import history from "connect-history-api-fallback";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  appType: "spa",
   server: {
     host: "::",
     port: 8080,
@@ -11,7 +13,18 @@ export default defineConfig(() => ({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "history-fallback",
+      configureServer(server) {
+        server.middlewares.use(history());
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use(history());
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
